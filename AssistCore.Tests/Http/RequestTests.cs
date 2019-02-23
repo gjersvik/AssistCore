7e4f479e2742a7c44ab22b6efd4a0afd1fb0a0fd
Ole Martin Gjersvik <olem@olem.org>
2019-02-23 14:00:47 +0100

    Easy way to add headers to Requests.

--- AssistCore.Tests/Http/RequestTests.cs
+++ AssistCore.Tests/Http/RequestTests.cs
@@ -8,14 +8,33 @@ namespace AssistCore.Tests.Http
     public class RequestTests
     {
         [Test]
-        public void Constructor(){
-            var req = new Request("GET", new Uri("file://test"),ImmutableDictionary<string,string>.Empty, ImmutableArray<byte>.Empty );
-            Assert.Multiple(() => {
+        public void Constructor()
+        {
+            var req = new Request("GET", new Uri("file://test"), ImmutableDictionary<string, string>.Empty, ImmutableArray<byte>.Empty);
+            Assert.Multiple(() =>
+            {
                 Assert.That(req.Method, Is.EqualTo("GET"), "Method");
                 Assert.That(req.Uri, Is.EqualTo(new Uri("file://test")), "Uri");
-                Assert.That(req.Headers, Is.EqualTo(ImmutableDictionary<string,string>.Empty), "Headers");
+                Assert.That(req.Headers, Is.EqualTo(ImmutableDictionary<string, string>.Empty), "Headers");
                 Assert.That(req.Body, Is.EqualTo(ImmutableArray<byte>.Empty), "Body");
             });
+        }
+        [Test]
+        public void Get()
+        {
+            var req = Request.Get("file://test");
+            Assert.Multiple(() =>
+            {
+                Assert.That(req.Method, Is.EqualTo("GET"), "Method");
+                Assert.That(req.Uri, Is.EqualTo(new Uri("file://test")), "Uri");
+            });
+        }
+        [Test]
+        public void SetHeader()
+        {
+            var req = Request.Get("file://test");
+            req = req.SetHeader("test", "testvalue");
+            Assert.That(req.Headers["test"], Is.EqualTo("testvalue"));
         }
     }
 }
--- AssistCore/Http/Request.cs
+++ AssistCore/Http/Request.cs
@@ -5,19 +5,25 @@ namespace AssistCore.Http
 {
     public class Request
     {
-        public static Request Get(string uri) => new Request("GET", new Uri(uri), ImmutableDictionary<string, string>.Empty, ImmutableArray<byte>.Empty);
+        public static Request Get(string uri) => new Request("GET", new Uri(uri));
 
         public readonly string Method;
         public readonly Uri Uri;
         public readonly ImmutableDictionary<string, string> Headers;
         public readonly ImmutableArray<byte> Body;
 
-        public Request(string method, Uri uri, ImmutableDictionary<string, string> headers, ImmutableArray<byte> body)
+        public Request(string method, Uri uri, ImmutableDictionary<string, string> headers = null, ImmutableArray<byte> body = default(ImmutableArray<byte>))
         {
             Method = method;
             Uri = uri;
-            Headers = headers;
+            Headers = headers ?? ImmutableDictionary<string, string>.Empty;
             Body = body;
+        }
+
+        public Request SetHeader(string key, string value)
+        {
+            var headers = Headers.SetItem(key, value);
+            return new Request(Method, Uri, headers, Body);
         }
     }
 }
