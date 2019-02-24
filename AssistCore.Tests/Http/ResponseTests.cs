7c60e592257599eac6bead2e3ba3fe985b7fd83a
Ole Martin Gjersvik <olem@olem.org>
2019-02-24 19:43:04 +0100

    Added some unittests.

--- AssistCore.Tests/Http/ResponseTests.cs
+++ AssistCore.Tests/Http/ResponseTests.cs
@@ -17,5 +17,31 @@ namespace AssistCore.Tests.Http
                 Assert.That(res.Body, Is.EqualTo(ImmutableArray<byte>.Empty), "Body");
             });
         }
+        [Test]
+        public void GatewayTimeout(){
+            var res = Response.GatewayTimeout;
+            Assert.Multiple(() => {
+                Assert.That(res.StatusCode, Is.EqualTo(504), "StatusCode");
+                Assert.That(res.Reason, Is.EqualTo("Gateway Timeout"), "Reason");
+            });
+        }
+        [Test]
+        public void NotFound(){
+            var res = Response.NotFound;
+            Assert.Multiple(() => {
+                Assert.That(res.StatusCode, Is.EqualTo(404), "StatusCode");
+                Assert.That(res.Reason, Is.EqualTo("Not Found"), "Reason");
+            });
+        }
+        [Test]
+        public void Text(){
+            var res = Response.Text("Test");
+            Assert.Multiple(() => {
+                Assert.That(res.StatusCode, Is.EqualTo(200));
+                Assert.That(res.Reason, Is.EqualTo("OK"));
+                Assert.That(res.Headers["Content-Type"], Is.EqualTo("text/plain; charset=utf-8"));
+                Assert.That(res.Body, Is.EqualTo(new byte[]{84, 101, 115, 116}).AsCollection);
+            });
+        }
     }
 }
